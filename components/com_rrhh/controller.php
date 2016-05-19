34f0b135761f3b86a2063aad18e68e8011a1b14b
Andrés Cortés <acortes@tfcons.com>
2016-05-19 07:11:33 -0500

    RRHH-0014
    Permisos Controlador

--- components/com_rrhh/controller.php
+++ components/com_rrhh/controller.php
@@ -16,30 +16,184 @@ jimport('joomla.application.component.controller');
 class RrhhController extends JControllerLegacy
 {
 
-	public function display($cachable = true, $urlparams = false)
-	{
-		$user = JFactory::getUser();
+	public function display($cachable = true, $urlparams = false){
+				
+		$document	= JFactory::getDocument();
 
 		// Set the default view name and format from the Request.
-		$vName = $this->input->get('view', 'tags');
-		$this->input->set('view', $vName);
+		$vName	 = JRequest::getCmd('view', 'cargos');
+		$vFormat = $document->getType();
+		$lName	 = JRequest::getCmd('layout', 'default');
 
-		if ($user->get('id') ||($this->input->getMethod() == 'POST' && $vName = 'tags'))
-		{
-			$cachable = false;
-		}
+		if ($view = $this->getView($vName, $vFormat)) {
+			// Do any specific processing by view.
+			if($vName != "close"){
+			
+				switch ($vName) {
+					
+					case 'areas': 
+						
+						if(!JFactory::getUser()->authorise('core.manage', 'com_rrhh')){
+							$this->setRedirect(JRoute::_('index.php', false));
+							return;
+						}//If the user is already logged in, redirect to the profile page.
+						
+						// The user is a guest, load the registration model and show the registration page.
+						if($lName == 'default'){
+							$model = $this->getModel('areas');
+						}else{
+							$model = $this->getModel('area');
+						}
+						break;
+							
+					case 'cargos':
+					
+						if(!JFactory::getUser()->authorise('core.manage', 'com_rrhh')) {
 
-		$safeurlparams = array(
-			'id'               => 'ARRAY',
-			'type'             => 'ARRAY',
-			'limit'            => 'UINT',
-			'limitstart'       => 'UINT',
-			'filter_order'     => 'CMD',
-			'filter_order_Dir' => 'CMD',
-			'lang'             => 'CMD'
-		);
+							$this->setRedirect(JRoute::_('index.php', false));
+							return;
 
-		return parent::display($cachable, $safeurlparams);
+						}//If the user is already logged in, redirect to the profile page.
+						
+						// The user is a guest, load the registration model and show the registration page.
+						if($lName == 'default'){
+							$model = $this->getModel('cargos');
+						}else{
+							$model = $this->getModel('cargo');
+						}
+						break;
+						
+					case 'empleados':
+					
+						if(!JFactory::getUser()->authorise('core.manage', 'com_rrhh')) {
+
+							$this->setRedirect(JRoute::_('index.php', false));
+							return;
+
+						}//If the user is already logged in, redirect to the profile page.
+						
+						// The user is a guest, load the registration model and show the registration page.
+						if($lName == 'default'){
+							$model = $this->getModel('empleados');
+						}else{
+							$model = $this->getModel('empleado');
+						}
+						break;
+					
+					case 'funcionarios':
+					
+						if(!JFactory::getUser()->authorise('core.manage', 'com_rrhh')) {
+
+							$this->setRedirect(JRoute::_('index.php', false));
+							return;
+
+						}//If the user is already logged in, redirect to the profile page.
+						
+						// The user is a guest, load the registration model and show the registration page.
+						if($lName == 'default'){
+							$model = $this->getModel('funcionarios');
+						}else{
+							$model = $this->getModel('funcionario');
+						}
+						break;
+						
+					case 'rrhh':
+					
+						if(!JFactory::getUser()->authorise('core.manage', 'com_rrhh')) {
+
+							$this->setRedirect(JRoute::_('index.php', false));
+							return;
+
+						}//If the user is already logged in, redirect to the profile page.
+						
+						// The user is a guest, load the registration model and show the registration page.
+						$model = $this->getModel('rrhh');
+						
+						break;	
+					
+					case 'rrhh_cargo':
+					
+						if(!JFactory::getUser()->authorise('core.manage', 'com_rrhh')) {
+
+							$this->setRedirect(JRoute::_('index.php', false));
+							return;
+
+						}//If the user is already logged in, redirect to the profile page.
+						
+						// The user is a guest, load the registration model and show the registration page.
+						$model = $this->getModel('rrhh_cargo');
+						
+						break;
+					
+					case 'sucesion':
+					
+						if(!JFactory::getUser()->authorise('core.manage', 'com_rrhh')) {
+
+							$this->setRedirect(JRoute::_('index.php', false));
+							return;
+
+						}//If the user is already logged in, redirect to the profile page.
+						
+						// The user is a guest, load the registration model and show the registration page.
+						$model = $this->getModel('sucesion');
+						
+						break;
+					
+					case 'tags':
+					
+						if(!JFactory::getUser()->authorise('core.manage', 'com_rrhh')) {
+
+							$this->setRedirect(JRoute::_('index.php', false));
+							return;
+
+						}//If the user is already logged in, redirect to the profile page.
+						
+						// The user is a guest, load the registration model and show the registration page.
+						$model = $this->getModel('tags');
+						
+						break;
+					
+					case 'tag':
+					
+						if(!JFactory::getUser()->authorise('core.manage', 'com_rrhh')) {
+
+							$this->setRedirect(JRoute::_('index.php', false));
+							return;
+
+						}//If the user is already logged in, redirect to the profile page.
+						
+						// The user is a guest, load the registration model and show the registration page.
+						$model = $this->getModel('tag');
+						
+						break;
+					
+					
+					default:
+					
+						$this->setRedirect(JRoute::_('index.php', false));
+						
+						return;
+
+				}
+				
+				$view->setModel($model, true);
+				$view->setLayout($lName);
+
+				// Push document object into the view.
+				$view->assignRef('document', $document);
+				$view->display();
+				
+			}else{
+				
+				// Check for edit form.
+				parent::display();
+
+				return $this;
+				
+			}
+			// Push the model into the view (as default).
+			
+		}		
 	}
 
 }
